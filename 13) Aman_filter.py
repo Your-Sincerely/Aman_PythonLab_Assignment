fa45dc234a01f0dadb423159a660897cb2fe9983
Your-Sincerely <101339053+Your-Sincerely@users.noreply.github.com>
2023-04-11 12:06:14 +0530

    Update 13) Aman_filter.py

--- 13) Aman_filter.py
+++ 13) Aman_filter.py
@@ -1,11 +1,27 @@
-def fun(i):
+'''filter(function, sequence)
+
+Parameters:
+function: function that tests if each element of a 
+sequence true or not.
+
+sequence: sequence which needs to be filtered, it can 
+be sets, lists, tuples, or containers of any iterators.
+
+Returns:
+returns an iterator that is already filtered.
+'''
+
+def function(i):
     if i % 2 == 0:
         return i
     else:
         return False
 
 
-List = [1, 2, 3, 4, 5, 6]
-a = filter(fun, List)  # filters the elements without using loops
+sequence = [1, 2, 3, 4, 5, 6]
+
+  # filters the elements without using loops
+a = filter(function, sequence)
+
 for i in a:
     print(i)
