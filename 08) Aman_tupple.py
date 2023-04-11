b8783d1c5fd099858bb8462b1cdad37b7783ca84
Your-Sincerely <101339053+Your-Sincerely@users.noreply.github.com>
2023-04-11 10:47:24 +0530

    Update and rename 21) Aman_tupple.py to 08) Aman_tupple.py

--- 08) Aman_tupple.py
+++ 08) Aman_tupple.py
@@ -1,11 +1,25 @@
+'''Tuples are used to store multiple items in a single variable.
+
+Tuple is one of 4 built-in data types in Python used to store collections of data,
+the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+A tuple is a collection which is ordered and unchangeable.
+
+Tuples are written with round brackets.
+'''
+
 # Creating a tuple:
 Tuple = ("First index", 1, 2, 3, "AMAN", True, False, "Last index")
 
 # Functions in tuple:
-print("Length of tuple", len(Tuple))  # length of tuple
-print("Backward indexing : ", Tuple[-1])  # Backward indexing getting element
-print("Forward indexing : ", Tuple[0])  # Forward indexing getting element
-print("Slicing in tuple using forward indexing", Tuple[0:2])  # slicing in tuple
+  # length of tuple
+print("Length of tuple", len(Tuple)) 
+ # Backward indexing getting element
+print("Backward indexing : ", Tuple[-1]) 
+  # Forward indexing getting element
+print("Forward indexing : ", Tuple[0])  
+  # slicing in tuple
+print("Slicing in tuple using forward indexing", Tuple[0:2])
 print("Printing tuple", Tuple)  # printing tuple
 
 New_Tuple = (1, 2, 3, 4)  # New Tuple
