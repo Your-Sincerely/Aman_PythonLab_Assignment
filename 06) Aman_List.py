21e7bdaab8c4b8d9a3c7b9d9913af7e6f12b8c4c
Your-Sincerely <101339053+Your-Sincerely@users.noreply.github.com>
2023-04-11 10:26:40 +0530

    Update and rename 6) Aman_List.py to 06) Aman_List.py

--- 06) Aman_List.py
+++ 06) Aman_List.py
@@ -1,7 +1,8 @@
 # Creating a list
+
+#        0  1  2  3  4  5  6  7  8  9  Forward indexing
 List = [1, 2, 33, 4, 5, 6, 7, 8, 9, 10]
-#       0  1  2  3  4  5  6  7  8  9  Forward indexing
-#     -10 -9 -8 -7 -6 -5 -4 -3 -2 -1  Backward indexing
+#      -10 -9 -8 -7 -6 -5 -4 -3 -2 -1  Backward indexing
 
 print("Length of list:", len(List))  # Length of List
 print("Accessing item from list:", List[0])  # Accessing element by index
